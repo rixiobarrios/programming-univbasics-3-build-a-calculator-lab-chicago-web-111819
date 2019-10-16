c80e05802a7c5820dc1f1e668393b98eddb82d90
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-16 20:47:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,17 @@
 def addition(num1, num2)
-  addition = (num1+num2)
+  addition = (num1 + num2)
 end
 
 def subtraction(num1, num2)
-  substraction = (num1-numb2)
+  subtraction = (num1 - numb2)
 end
 
 def division(num1, num2)
-  division = (num1/num2)
+  division = (num1 / num2)
 end
 
 def multiplication(num1, num2)
-  multiplication = (num1*num2)
+  multiplication = (num1 * num2)
 end
 
 def modulo(num1, num2)
